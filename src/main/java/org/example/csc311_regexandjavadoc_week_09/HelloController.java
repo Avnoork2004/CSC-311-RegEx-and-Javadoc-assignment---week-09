32bcb58b65af501a13e262c86853a5f17685752c
Avnoor Kaur <Avnoork2004@gmail.com>
2024-10-24 18:45:26 -0400

    created textfield, labels and buttons for gui fxml files

--- src/main/java/org/example/csc311_regexandjavadoc_week_09/HelloController.java
+++ src/main/java/org/example/csc311_regexandjavadoc_week_09/HelloController.java
@@ -1,14 +1,37 @@
 package org.example.csc311_regexandjavadoc_week_09;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button addButton;
+
+    @FXML
+    private TextField dobField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField firstNameField;
+
+    @FXML
+    private TextField lastNameField;
+
+    @FXML
+    private Label validationMessage;
+
+    @FXML
+    private TextField zipField;
+
+    @FXML
+    void onHelloButtonClick(ActionEvent event) {
+
     }
+
 }
